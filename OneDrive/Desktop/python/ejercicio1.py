b488badd91ea6159d85c544838af2b66b0b6dde0
Catalina Flores <catafloress.007@gmail.com>
2026-03-18 00:41:38 -0300

    Agrego ejercicio1

---
+++ OneDrive/Desktop/python/ejercicio1.py
@@ -0,0 +1,10 @@
+anio = int(input("Ingrese su anio de nacimiento"))
+
+anio_18 = anio + 18 
+anio_21 = anio + 21 
+anio_100 = anio + 100
+
+print("Cumplis 18 en :", anio_18)
+print("Cumplis 21 en :", anio_21)
+print("Cumplis 100 en :", anio_100)
+ 
